930490458fd0cdedd709c5d3e7168c0118abc575
Robbi Nespu <robbinespu@gmail.com>
2021-04-17 17:04:04 +0800

    Initial commit

---
+++ src/test/java/com/robbi/nespu/examscheduler/ExamSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robbi.nespu.examscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
